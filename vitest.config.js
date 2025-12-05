faf7823979c3fd4250e724799d4cc21f77c5955d
Really Him <hesreallyhim@proton.me>
2025-12-04 22:14:10 -0500

    build: improve ci config

---
+++ vitest.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      reportsDirectory: "coverage",
+      exclude: [
+        "**/test/**",
+        "**/examples/**",
+        "**/docs/**",
+        "**/node_modules/**",
+        "vitest.config.{js,ts}",
+        "pre-vhs.config.{js,cjs,json}",
+      ],
+    },
+  },
+});
